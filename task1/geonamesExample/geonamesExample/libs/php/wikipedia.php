65593d9f7ca71a0238f89275f75a4c83d1ad10a9
Isaiah Ojo <51814402+LafyOjo@users.noreply.github.com>
2022-11-03 11:23:16 +0000

    Update wikipedia.php
    new file

--- task1/geonamesExample/geonamesExample/libs/php/wikipedia.php
+++ task1/geonamesExample/geonamesExample/libs/php/wikipedia.php
@@ -1,21 +1,31 @@
 <?php
-//"http://api.geonames.org/wikipediaSearchJSON?formatted=true&q=london&maxRows=10&username=isaiahojo&style=full";
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
 
-$url = "http://api.geonames.org/wikipediaSearchJSON?formatted=true&q=london&maxRows=10&username=isaiahojo&style=full";
- 
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
 
+    $executionStartTime = microtime(true);
 
-curl_setopt(curl_init(), CURLOPT_SSL_VERIFYPEER, false);
+    $urlpostcodes='http://api.geonames.org/postalCodeSearchJSON?formatted=true&postalcode=' . $_REQUEST['postalcode'] . '&maxRows=5&username=isaiahojo&style=full';
 
-curl_setopt(curl_init(), CURLOPT_RETURNTRANSFER, true);
+    $chpostcode = curl_init();
+    curl_setopt($chpostcode, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($chpostcode, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($chpostcode, CURLOPT_URL, $urlpostcodes);
 
-curl_setopt(curl_init(), CURLOPT_URL, $url);
+    $resultpostcodes = curl_exec($chpostcode);
 
-$result = curl_exec(curl_init());
+    curl_close($chpostcode);
 
-curl_close(curl_init());
+    $decodpostcodes = json_decode($resultpostcodes, true);
 
-$decode = json_decode($result, true);
-echo json_encode($decode);
+    $outputpostcodes['status']['code'] = '200';
+    $outputpostcodes['status']['name'] = 'ok';
+    $outputpostcodes['status']['description'] = 'success';
+    $outputpostcodes['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . ' ms';
+    $outputpostcodes['data'] = $decodpostcodes['postalCodes'];
+
+        
+    header('Content-Type: application/json; charset=UTF-8');
+    echo json_encode($outputpostcodes);
+
+?>
