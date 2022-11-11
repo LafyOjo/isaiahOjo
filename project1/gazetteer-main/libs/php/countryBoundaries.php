54daed2c93036596f8258be06efdde6920326d5c
Isaiah Ojo <51814402+LafyOjo@users.noreply.github.com>
2022-11-11 19:40:55 +0000

    Add files via upload

---
+++ project1/gazetteer-main/libs/php/countryBoundaries.php
@@ -0,0 +1,26 @@
+<?php
+
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+
+    $start=microtime( true );
+    $file=file_get_contents('countryBorders.geo.json');
+
+
+    $json = json_decode($file,true);
+
+    $output = [];
+    foreach ($json["features"] as $object) {
+        if ($object["properties"]["iso_a2"] == $_REQUEST['iso']) {
+            $output['status']['code'] = "200";
+            $output['status']['name'] = "ok";
+            $output['status']['description'] = "success";
+            $output['status']['returnedIn'] = intval((microtime(true) - $start) * 1000) . " ms";
+            $output['data'] = $object["geometry"];
+        };
+    };
+
+    header('Content-Type: application/json; charset=UTF-8');
+    echo json_encode($output); 
+    
+?>
