996fc94521c9e0b746e766d030c370a36340bb94
Isaiah Ojo <51814402+LafyOjo@users.noreply.github.com>
2022-11-20 13:54:21 +0000

    Uplaoded files

---
+++ project2/companydirectory/libs/php/config.php
@@ -0,0 +1,11 @@
+<?php
+
+	$cd_host = "127.0.0.1";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	$cd_user = "companydirectory"; // user name
+	$cd_password = "companydirectory"; // password
+	$cd_dbname = "companydirectory"; // database name
+
+?>
