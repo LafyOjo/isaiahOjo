e49020e15d813b99c5c55ba25f7c387cc50436d7
Isaiah Ojo <51814402+LafyOjo@users.noreply.github.com>
2022-11-25 23:14:08 +0000

    sql injection

--- project2/companydirectory/libs/php/SearchLocation.php
+++ project2/companydirectory/libs/php/SearchLocation.php
@@ -24,9 +24,13 @@
 
 	}	
 
-	$query = 'SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location FROM personnel p LEFT JOIN department d ON (d.id = p.departmentID) LEFT JOIN location l ON (l.id = d.locationID) WHERE l.id ='. $_REQUEST['name'];
+	$query = $conn->prepare('SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location FROM personnel p LEFT JOIN department d ON (d.id = p.departmentID) LEFT JOIN location l ON (l.id = d.locationID) WHERE l.id = ?');
+    
+    $query->bind_param("i", $_REQUEST['name']);
 
-	$result = $conn->query($query);
+	$query->execute();
+
+    $result = $query->get_result();
 	
 	if (!$result) {
 
